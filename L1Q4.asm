647c2537a1c56f1e30e0aed715f6c5719085c5a6
Camylla Oliveira <camyllaoliveira@users.noreply.github.com>
2017-08-09 21:51:09 -0300

    Ok

--- L1Q4.asm
+++ L1Q4.asm
@@ -1,7 +1,7 @@
 main:.
 	addi $a0, $zero, -1	# a0 = ...
 	addi $a1, $zero, 5	# a1 = ...
-	bgt $a0, $a1, error	# se a0 > a1, v· para error
+	bgt $a0, $a1, error	# se a0 > a1, v√° para error
 	add $v0, $zero, $a0	# v0 = valor inicial de a0
 	
 sum:
@@ -13,7 +13,6 @@ sumrecursive:
 	subi $a1, $a1, 1		# a1--
 	j sum				# chamada de sum
 	
-
 error:
 	addi $v1, $zero, 1	# v1 = 1
 	break			# encerra com erro
